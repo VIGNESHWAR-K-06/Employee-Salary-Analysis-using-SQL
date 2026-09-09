2803e743bad532183086ba9a191db3f5bdcfb9b6
VIGNESHWAR K <102378199+VIGNESHWAR-K-06@users.noreply.github.com>
2026-09-10 01:46:55 +0530

    Add employee records to the database
    Inserted multiple employee records into the employees table.

---
+++ sql/sql/02_insert_data.sql
@@ -0,0 +1,18 @@
+INSERT INTO employees
+(employee_id, name, department, job_role, salary, experience_years, location)
+VALUES
+(101, 'Vignesh', 'IT', 'Support Engineer', 65000, 5, 'Bangalore'),
+(102, 'Kedar', 'HR', 'HR Executive', 45000, 3, 'Chennai'),
+(103, 'Varshini', 'IT', 'Developer', 75000, 6, 'Bangalore'),
+(104, 'Divya', 'Finance', 'Financial Analyst', 60000, 4, 'Chennai'),
+(105, 'Karthik', 'IT', 'Support Engineer', 58000, 4, 'Coimbatore'),
+(106, 'Sneha', 'HR', 'HR Manager', 70000, 7, 'Bangalore'),
+(107, 'Vijay', 'Finance', 'Accountant', 50000, 5, 'Madurai'),
+(108, 'Anitha', 'IT', 'Data Analyst', 72000, 5, 'Chennai'),
+(109, 'Suresh', 'IT', 'Developer', 80000, 8, 'Bangalore'),
+(110, 'Meena', 'Finance', 'Financial Analyst', 62000, 6, 'Coimbatore'),
+(111, 'Ravi', 'IT', 'Support Engineer', 55000, 3, 'Chennai'),
+(112, 'Kavya', 'HR', 'HR Executive', 48000, 2, 'Madurai'),
+(113, 'Manoj', 'IT', 'Data Analyst', 68000, 4, 'Bangalore'),
+(114, 'Pooja', 'Finance', 'Accountant', 52000, 3, 'Chennai'),
+(115, 'Ganesh', 'IT', 'Support Engineer', 60000, 5, 'Coimbatore');
