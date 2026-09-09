3b0a38ad7c977e4172b0e3430eaaafe06e818fe1
VIGNESHWAR K <102378199+VIGNESHWAR-K-06@users.noreply.github.com>
2026-09-10 01:45:12 +0530

    Update 01_create_table.sql

--- sql/01_create_table.sql
+++ sql/01_create_table.sql
@@ -1,2 +1,13 @@
 Employee Salary-Analysis using SQL
 SQL project for analyzing employee salary and department data
+
+
+CREATE TABLE employees (
+    employee_id INT,
+    name VARCHAR(50),
+    department VARCHAR(50),
+    job_role VARCHAR(50),
+    salary INT,
+    experience_years INT,
+    location VARCHAR(50)
+);
